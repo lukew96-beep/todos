a6c81cedee901a4cfaa23027915708c08d89945c
lukew96-beep <56884537+lukew96-beep@users.noreply.github.com>
2021-09-16 02:39:10 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+const todoForm = document.querySelector('form');
+const todoList = document.getElementById('results');
+const savedTodos = JSON.parse(localStorage.getItem("todos")) || [];
+for (let i = 0; i < savedTodos.length; i++){
+  const newTodo = document.createElement('li');
+  newTodo.innerText = savedTodos[i].task;
+  newTodo.isCompleted = savedTodos[i].isCompleted ? true: false;
+  if(newTodo.isCompleted){
+    newTodo.style.textDecoration = "line-through";
+    todoList.appendChild(newTodo)
+  }
+}
+todoForm.addEventListener('submit', function(e){
+    e.preventDefault();
+    
+    const removeButton = document.createElement('button');
+    removeButton.innerText = "X";
+
+    let newTodo = document.createElement('li');
+    newTodo.innerText = todoInput.value;
+
+    todoList.appendChild(newTodo);
+    newTodo.appendChild(removeButton);
+    todoForm.reset;
+
+    savedTodos.push({task: newTodo.innerText, isCompleted: false});
+    localStorage.setItem("todos", JSON.stringify(savedTodos))
+});
+
+
+todoList.addEventListener("click", function(event) {
+    const targetTagToLowerCase = event.target.tagName.toLowerCase();
+    if (targetTagToLowerCase === "li") {
+      event.target.style.textDecoration = "line-through";
+    } else if (targetTagToLowerCase === "button") {
+      event.target.parentNode.remove();
+    }
+  });
+  
