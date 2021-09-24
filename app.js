174afa14c27056f695266f9528da3792901e52a0
lukew96-beep <56884537+lukew96-beep@users.noreply.github.com>
2021-09-24 11:02:31 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,10 +1,40 @@
-const memeForm = document.querySelector('form');
-const imageInput = document.querySelector('#imgUrl');
-const topText = document.querySelector('#topText');
-const bottomText = document.querySelector('#bottomText');
-const results = document.querySelector('#results');
-memeForm.addEventListener('submit', function(){
-    const memeImg = document.createElement('img');
-    memeImg.src = imageInput.value;
-    results.appendChild('memeImg');
-})
+const todoForm = document.querySelector('form');
+const todoList = document.getElementById('results');
+const savedTodos = JSON.parse(localStorage.getItem("todos")) || [];
+for (let i = 0; i < savedTodos.length; i++){
+  const newTodo = document.createElement('li');
+  newTodo.innerText = savedTodos[i].task;
+  newTodo.isCompleted = savedTodos[i].isCompleted ? true: false;
+  if(newTodo.isCompleted){
+    newTodo.style.textDecoration = "line-through";
+  }
+  todoList.appendChild(newTodo);
+  const newBtn = newTodo.appendChild(document.createElement('button'));
+}
+todoForm.addEventListener('submit', function(e){
+    e.preventDefault();
+    
+    const removeButton = document.createElement('button');
+    removeButton.innerText = "X";
+
+    let newTodo = document.createElement('li');
+    newTodo.innerText = todoInput.value;
+
+    todoList.appendChild(newTodo);
+    newTodo.appendChild(removeButton);
+    todoForm.reset;
+
+    savedTodos.push({task: newTodo.innerText, isCompleted: false});
+    localStorage.setItem("todos", JSON.stringify(savedTodos))
+});
+
+
+todoList.addEventListener("click", function(event) {
+    const targetTagToLowerCase = event.target.tagName.toLowerCase();
+    if (targetTagToLowerCase === "li") {
+      event.target.style.textDecoration = "line-through";
+    } else if (targetTagToLowerCase === "button") {
+      event.target.parentNode.remove();
+    }
+  });
+  
